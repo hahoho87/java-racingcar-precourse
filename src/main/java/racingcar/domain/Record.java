fb4a703ac899d4012e2e3a98b2dc8fa19f1a3d08
hahoho87 <bbaing4564@gmail.com>
2022-10-06 23:05:52 +0900

    chore: 클래스명 변경 (Result -> Record)

--- src/main/java/racingcar/domain/Record.java
+++ src/main/java/racingcar/domain/Record.java
@@ -1,17 +1,17 @@
 package racingcar.domain;
 
-public class Result {
+public class Record {
 
 	private final String name;
 	private final int position;
 
-	private Result(String name, int position) {
+	private Record(String name, int position) {
 		this.name = name;
 		this.position = position;
 	}
 
-	public static Result from(RacingCar racingCar) {
-		return new Result(racingCar.getName(), racingCar.getPosition());
+	public static Record from(RacingCar racingCar) {
+		return new Record(racingCar.getName(), racingCar.getPosition());
 	}
 
 	public String getName() {
